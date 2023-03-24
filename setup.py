b84e7792dc27578c0fa92a970fbc144cbadb82d2
Carlos Segura <cseguramail@gmail.com>
2023-03-25 00:24:00 +0100

    Bump up version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='cbpro',
-    version='1.1.4',
+    version='1.1.5',
     author='Daniel Paquin',
     author_email='dpaq34@gmail.com',
     license='MIT',
